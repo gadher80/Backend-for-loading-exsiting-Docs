45ae6ae21c234d76f497cb6f3b109c5675acae5d
Hardik Gadher <61086577+gadher80@users.noreply.github.com>
2022-06-03 12:57:09 +0200

    Add files via upload

--- Tkinterpro02.py
+++ Tkinterpro02.py
@@ -1,36 +1,53 @@
+from time import strftime
 from tkinter import *
 
-root = Tk()
-root.geometry("300x300")
-root.title(" Q&A ")
+from TkinterPRo01 import MainUI
+
+UsernameList = ['Hardik007','Jagrat009','Jay003','Varun369']
+passwordList =['hardik007','jagrat009', 'jay003','varun369']
+def validateLogin(username,password) :
+    print("username entered :",username.get())
+    print("password entered :",password.get())
+    return
+
+tkWindow = Tk()
+tkWindow.geometry('450x450')
+tkWindow.title('Tkinter Login Form - pythonexamples.org')
+
+Label(tkWindow,text='Data Extraction GUI',font=("Raleway",30),fg="Black",height=2,width=20).place(x=5,y=40)
+
+def myTime():
+    time_string = strftime('%Y-%m-%d %H:%M:%S')  # time format
+    l1.config(text=time_string)
+    l1.after(1000,myTime)  # time delay of 1000 milliseconds
+
+myfont = ('Raleway',25,'bold')
+l1 = Label(tkWindow,font=myfont)
+l1.place(x=70,y=350)
+myTime()
+
+# username label and text entry box
+Label(tkWindow,font=("Raleway",18),text="User Name").place(x=10,y=150)
+username = StringVar()
+usernameEntry = Entry(tkWindow,textvariable=username,font="Raleway",borderwidth=6,relief="groove",bg="#FFFFFF",
+                      fg="black",width=20)
+usernameEntry.place(x=150,y=150)
+
+# password label and password entry box
+Label(tkWindow,font=("Raleway",18),text="Password").place(x=10,y=200)
+password = StringVar()
+passwordEntry = Entry(tkWindow,textvariable=password,font="Raleway",borderwidth=6,relief="groove",bg="#FFFFFF",
+                      fg="black",width=20)
+passwordEntry.place(x=150,y=200)
+
+validateLogin = partial(validateLogin,username,password)
+
+# login button
+
+Button(tkWindow,text="Login",font=("Raleway",15),borderwidth=6,command=lambda : MainUI.PressedLogin(),height=1,
+       width=10).place(x=250,y=270)
+tkWindow.mainloop()
+tkWindow.withdraw()
 
 
-def Take_input() :
-    INPUT = inputtxt.get("1.0","end-1c")
-    print(INPUT)
-    if (INPUT == "120") :
-        Output.insert(END,'Correct')
-    else :
-        Output.insert(END,"Wrong answer")
 
-
-l = Label(text="What is 24 * 5 ? ")
-inputtxt = Text(root,height=10,
-                width=25,
-                bg="light yellow")
-
-Output = Text(root,height=5,
-              width=25,
-              bg="light cyan")
-
-Display = Button(root,height=2,
-                 width=20,
-                 text="Show",
-                 command=lambda : Take_input())
-
-l.pack()
-inputtxt.pack()
-Display.pack()
-Output.pack()
-
-mainloop()
